52f23a06ef1cdab20950ac64b66d3cae8027fb2b
arshedgithub <arshedahmedmail@gmail.com>
2021-05-05 10:30:10 +0530

    first commit

---
+++ guiCalculator.java
@@ -0,0 +1,11 @@
+public class guiCalculator{
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+
+        frame.setTitle("Calculator");
+        frame.setSize(200,300);
+        Container container = frame.getContentPane(); 
+
+    }
+}
