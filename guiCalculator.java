6ed216809955649170e3b8f2b3c20637293997a1
arshedgithub <arshedahmedmail@gmail.com>
2021-05-05 11:32:50 +0530

    create jframe

--- guiCalculator.java
+++ guiCalculator.java
@@ -1,11 +1,25 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
 public class guiCalculator{
     public static void main(String[] args) {
         
         JFrame frame = new JFrame();
 
         frame.setTitle("Calculator");
-        frame.setSize(200,300);
+        frame.setLocation(100,250);
+        frame.setSize(500,300);
+
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+
         Container container = frame.getContentPane(); 
+
+        frame.setVisible(true);
 
     }
 }
